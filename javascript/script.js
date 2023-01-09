8d0af995e34d6c67c224688ea1510528671bef08
Thiago Corrêa <thiagocorreacontato@gmail.com>
2023-01-09 10:49:10 -0300

    feat: created function to store a book on an array

---
+++ javascript/script.js
@@ -0,0 +1,17 @@
+let myLibrary = [];
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+function addBookToLibrary(title, author, pages, read) {
+    const book = new Book(title, author, pages, read);
+    myLibrary.push(book);
+}
+
+addBookToLibrary("The Hobbit by J.R.R.", "Tolkien", 295, false);
+
+console.log(myLibrary);
